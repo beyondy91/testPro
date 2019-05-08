937dda00393cd781c0444b8a40dbbd9c0178c00b
peter <peter@genoplan.com>
2019-05-08 13:59:34 +0900

    test.java

--- java/code/test.java
+++ java/code/test.java
@@ -1,1 +1,10 @@
 /* This is a test */
+
+public class test {
+
+   public static void main(String[] args) {
+	// TODO Auto-generated method stub
+        System.out.println("Hello JAVA");
+   }
+
+}
